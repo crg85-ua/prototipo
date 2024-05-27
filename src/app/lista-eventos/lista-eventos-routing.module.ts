9d5b645285fd270534dfa7e5b7f6626fb6ade4a6
crg85-ua <crg85@gcloud.ua.es>
2024-05-27 18:58:38 +0200

    detalle, listado, crear

---
+++ src/app/lista-eventos/lista-eventos-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ListaEventosPage } from './lista-eventos.page';
+
+const routes: Routes = [
+  {
+    path: 'lista-eventos',
+    component: ListaEventosPage
+  },  
+  {
+    path: 'detalle-evento/:id',
+    loadChildren: () => import('../detalle-evento/detalle-evento.module').then(m => m.DetalleEventoPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ListaEventosPageRoutingModule {}
---
+++ src/app/models/evento.ts
@@ -0,0 +1,9 @@
+export class Evento {
+    id: string="";
+    name: string="";
+    description: string="";
+    startDate: string="";
+    host: string="";
+    location: string="";
+    photo: string="";
+}
