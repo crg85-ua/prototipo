c9063aced8893ab95e92f4a9f62cf091a75f9791
jao9-ua <jao9@alu.ua.es>
2024-05-27 13:10:54 +0200

    creada pagina de user

---
+++ src/app/perfil/perfil.page.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil',
+  templateUrl: './perfil.page.html',
+  styleUrls: ['./perfil.page.scss'],
+})
+export class PerfilPage {
+  isEditing: boolean = false;
+  showMoreInterests: boolean = false;
+  profileDescription: string = "Soy un apasionado de la música y me encanta descubrir nuevos talentos. Siempre estoy en busca de nuevas experiencias y me encanta compartir mis intereses con otros.";
+  editableDescription: string = this.profileDescription;
+  interests: any[] = [
+    { name: "Sesiones de música", image: "../../assets/images/perfil/FotoInteres.jpg", selected: true },
+    { name: "DJs", image: "../../assets/images/perfil/Djs.jpg", selected: true },
+    { name: "Fiestas", image: "../../assets/images/perfil/Fiesta.jpg", selected: true }
+  ];
+  moreInterests: any[] = [
+    { name: "Senderismo", image: "../../assets/images/perfil/senderismo.jpeg", selected: false },
+    { name: "Lectura", image: "../../assets/images/perfil/lectura.jfif", selected: false }
+  ];
+
+  constructor() {}
+
+  editDescription() {
+    this.editableDescription = this.profileDescription;
+    this.isEditing = true;
+  }
+
+  saveDescription() {
+    this.profileDescription = this.editableDescription;
+    this.isEditing = false;
+  }
+
+  cancelEdit() {
+    this.isEditing = false;
+  }
+
+  toggleMoreInterests() {
+    this.showMoreInterests = true;
+    this.isEditing = true;
+  }
+
+  addInterest(interest: any) {
+    interest.selected = true;
+    this.interests.push(interest);
+    this.moreInterests = this.moreInterests.filter(item => item.name !== interest.name);
+  }
+
+  removeInterest(interest: any) {
+    interest.selected = false;
+    this.interests = this.interests.filter(item => item.name !== interest.name);
+    if (!this.moreInterests.some(item => item.name === interest.name)) {
+      this.moreInterests.push(interest);
+    }
+  }
+
+  goBack() {
+    this.showMoreInterests = false;
+    this.moreInterests = this.moreInterests.filter(interest => !this.interests.some(i => i.name === interest.name));
+    this.isEditing = false;
+  }
+}
